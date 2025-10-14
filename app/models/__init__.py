dd21bb3ff57c70ec5ff55a56f21509c5070b55e8
x0rtex <s00253368@atu.ie>
2025-10-14 22:02:39 +0100

    Exporting data models

---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+from app.models.user_model import (
+    UserModel,
+    UserCollection,
+)
+from app.models.vault_model import (
+    VaultModel,
+    VaultCollection,
+)
+
+__all__ = [
+    "UserModel",
+    "UserCollection",
+    "VaultModel",
+    "VaultCollection",
+]
