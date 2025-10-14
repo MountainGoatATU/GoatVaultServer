bddfd98146b0c95d5de50342f3caad6f4984e67f
x0rtex <s00253368@atu.ie>
2025-10-14 23:05:05 +0100

    Renamed vaults and users to vault_collection and user_collection

--- app/database.py
+++ app/database.py
@@ -8,5 +8,5 @@ _ = load_dotenv()
 client = AsyncMongoClient(os.environ["MONGODB_URL"])
 db = client.get_database(os.environ["DATABASE_NAME"])
 
-vaults = db.get_collection("vaults")
-users = db.get_collection("users")
+vault_collection = db.get_collection("vaults")
+user_collection = db.get_collection("users")
