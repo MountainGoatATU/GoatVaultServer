5f24bfdb14ada39499b0af11d8d5670725a38b6e
x0rtex <s00253368@atu.ie>
2025-10-15 00:26:47 +0100

    Added middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from starlette.middleware.errors import ServerErrorMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from starlette.middleware.exceptions import ExceptionMiddleware
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from app.routes import user_route, vault_route
 
+
 _ = load_dotenv()
 app = FastAPI(title="GoatVaultServer", description="A server for GoatVault")
+
+app.add_middleware(ServerErrorMiddleware)
+# app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
+app.add_middleware(ExceptionMiddleware)
+
 
 app.include_router(user_route.router, prefix="/v1")
 app.include_router(vault_route.router, prefix="/v1")
