c3d3833a467253fc36a57c123f2768acdf52c3eb
x0rtex <s00253368@atu.ie>
2025-10-14 23:05:46 +0100

    Extracted database logic and routers to seperate files

--- app/main.py
+++ app/main.py
@@ -1,24 +1,18 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from pymongo import AsyncMongoClient
-from pymongo import ReturnDocument
 from dotenv import load_dotenv
-import os
+from fastapi import FastAPI
+
+from app.routes import user_route, vault_route
 
 _ = load_dotenv()
 app = FastAPI(title="GoatVaultServer", description="A server for GoatVault")
-client = AsyncMongoClient(os.environ["MONGODB_URL"])
-db = client.get_database(os.environ["DATABASE_NAME"])
 
-vaults = db.get_collection("vaults")
-users = db.get_collection("users")
+app.include_router(user_route.router, prefix="/v1")
+app.include_router(vault_route.router, prefix="/v1")
 
 
-@app.get("/vaults/{user_id}")
-def read_vaults(user_id: int):
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/")
+async def root():
+    """
+    Root endpoint for GoatVault Server.
+    """
+    return {"message": "GoatVault Root Endpoint"}
