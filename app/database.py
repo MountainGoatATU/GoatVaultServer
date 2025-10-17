ccd74a72219a37d5d8ffcd2f04ef948590d4b1ee
x0rtex <s00253368@atu.ie>
2025-10-17 17:09:42 +0100

    Added validation for environment variables

--- app/database.py
+++ app/database.py
@@ -5,8 +5,16 @@ from pymongo import AsyncMongoClient
 
 _ = load_dotenv()
 
-client = AsyncMongoClient(os.environ["MONGODB_URL"], uuidRepresentation="standard")
-db = client.get_database(os.environ["DATABASE_NAME"])
+MONGODB_URL = os.getenv("MONGODB_URL")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+if not MONGODB_URL or not DATABASE_NAME:
+    raise ValueError(
+        "Missing required environment variables: MONGODB_URL and DATABASE_NAME"
+    )
+
+client = AsyncMongoClient(MONGODB_URL, uuidRepresentation="standard")
+db = client.get_database(DATABASE_NAME)
 
 vault_collection = db.get_collection("vaults")
 user_collection = db.get_collection("users")
