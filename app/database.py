08926ab98679af1edea2622247b333a906664f4d
x0rtex <s00253368@atu.ie>
2025-10-17 17:15:25 +0100

    Added type hinting

--- app/database.py
+++ app/database.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 from pymongo import AsyncMongoClient
+from pymongo.asynchronous.database import AsyncDatabase
+from pymongo.asynchronous.collection import AsyncCollection
 
 _ = load_dotenv()
 
@@ -13,8 +15,8 @@ if not MONGODB_URL or not DATABASE_NAME:
         "Missing required environment variables: MONGODB_URL and DATABASE_NAME"
     )
 
-client = AsyncMongoClient(MONGODB_URL, uuidRepresentation="standard")
-db = client.get_database(DATABASE_NAME)
+client: AsyncMongoClient = AsyncMongoClient(MONGODB_URL, uuidRepresentation="standard")
+db: AsyncDatabase = client.get_database(DATABASE_NAME)
 
-vault_collection = db.get_collection("vaults")
-user_collection = db.get_collection("users")
+vault_collection: AsyncCollection = db.get_collection("vaults")
+user_collection: AsyncCollection = db.get_collection("users")
