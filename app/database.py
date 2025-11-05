08969ad787df23505a499b9e4a7b44aba21e47d6
x0rtex <s00253368@atu.ie>
2025-11-05 19:55:46 +0000

    Added two indexes to database

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from pymongo import AsyncMongoClient
-from pymongo.asynchronous.database import AsyncDatabase
+from pymongo import ASCENDING, AsyncMongoClient, IndexModel
 from pymongo.asynchronous.collection import AsyncCollection
+from pymongo.asynchronous.database import AsyncDatabase
 
 _ = load_dotenv()
 
@@ -20,3 +20,25 @@ db: AsyncDatabase = client.get_database(DATABASE_NAME)
 
 vault_collection: AsyncCollection = db.get_collection("vaults")
 user_collection: AsyncCollection = db.get_collection("users")
+
+
+async def create_indexes() -> None:
+    """
+    Create database indexes for optimal query performance.
+
+    This function should be called during application startup.
+    """
+    # User collection indexes
+    user_indexes = [
+        IndexModel([("email", ASCENDING)], unique=True, name="email_unique_idx"),
+    ]
+    await user_collection.create_indexes(user_indexes)
+
+    # Vault collection indexes
+    vault_indexes = [
+        IndexModel([("user_id", ASCENDING)], name="user_id_idx"),
+        IndexModel(
+            [("user_id", ASCENDING), ("_id", ASCENDING)], name="user_id_vault_id_idx"
+        ),
+    ]
+    await vault_collection.create_indexes(vault_indexes)
