68295afa5867a93802f356c43e3283dd12cd4be8
Jakub-Lukac <jakub.lukac81@gmail.com>
2026-01-12 16:45:21 +0000

    added permission role to lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy_attachment" "vpc_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 # ==================================
 # VPC + Subnets + Internet/NAT
 # ==================================
