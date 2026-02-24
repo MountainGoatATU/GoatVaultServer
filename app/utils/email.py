aba6e19939dd54ef6ae71c6f9db9ee2fd39945da
x0rtex <s00253368@atu.ie>
2026-02-24 13:34:58 +0000

    email config

--- app/utils/email.py
+++ app/utils/email.py
@@ -6,7 +6,7 @@ BASE_URL = os.environ.get("LOCAL_SERVER_URL")
 conf = ConnectionConfig(
     MAIL_USERNAME=os.environ.get("EMAIL_USERNAME"),
     MAIL_PASSWORD=os.environ.get("EMAIL_PASSWORD"),
-    MAIL_FROM="no-reply@mountaingoat.dev",
+    MAIL_FROM="os.environ.get("EMAIL_USERNAME")",
     MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
     MAIL_STARTTLS=True,
@@ -33,7 +33,7 @@ async def send_verification(to_email: str, token: str):
             </body>
         </html>
         """,
-        subtype="html",
+        subtype="html"
     )
     fm = FastMail(conf)
     await fm.send_message(message)
